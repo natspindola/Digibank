3a9e0aed497a29ecaa0bb8894dfb2a6065873a4e
natspindola <natspindola@hotmail.com>
2022-04-11 19:05:11 -0300

    classe banco

---
+++ Digibank/Classes/Banco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digibank.Classes
+{
+    public abstract class Banco
+    {
+        public Banco()
+        {
+            this.NomeDoBanco = "Digibank";
+            this.CodigoDoBanco = "027";
+        }
+
+        public string NomeDoBanco { get; private set; }
+        public string CodigoDoBanco { get; private set; }
+    }
+}
