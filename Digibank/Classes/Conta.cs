b699d45977e71ec2e50898402cfdcd29104468b6
natspindola <natspindola@hotmail.com>
2022-04-11 19:07:54 -0300

    classe conta

---
+++ Digibank/Classes/Conta.cs
@@ -0,0 +1,42 @@
+﻿using Digibank.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digibank.Classes
+{
+    public abstract class Conta : Banco, IConta
+    {
+        public double ConsultaSalto()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deposita(double valor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCodigoDoBanco()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetNumeroAgencia()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetNumeroConta()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Saca(double valor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
