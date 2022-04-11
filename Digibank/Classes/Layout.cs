cc54f9f195a4d7ac3d928fe52e0013f35a47ef3d
natspindola <natspindola@hotmail.com>
2022-04-11 18:08:28 -0300

    layout pt1

---
+++ Digibank/Classes/Layout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digibank.Classes
+{
+    public class Layout
+    {
+        public static void TelaPrincipal()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
