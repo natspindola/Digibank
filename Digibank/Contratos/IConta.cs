85c073187af0d9a253580ead1f0271c7ead745c7
natspindola <natspindola@hotmail.com>
2022-04-11 19:02:57 -0300

    intarface

---
+++ Digibank/Contratos/IConta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digibank.Contratos
+{
+    public interface IConta
+    {
+        void Deposita(double valor);
+        bool Saca(double valor);
+        double ConsultaSalto();
+        string GetCodigoDoBanco();
+        string GetNumeroAgencia();
+        string GetNumeroConta();
+    }
+}
