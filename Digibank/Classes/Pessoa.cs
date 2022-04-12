4b13f78668b4c3ddedb92b3d7d4877879bdf0425
natspindola <natspindola@hotmail.com>
2022-04-12 19:08:56 -0300

    classe pessoa

---
+++ Digibank/Classes/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using Digibank.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digibank.Classes
+{
+    class Pessoa
+    {
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Senha { get; private set; }
+        public IConta Conta { get; set; }
+    }
+}
