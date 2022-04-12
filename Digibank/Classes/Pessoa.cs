47f2887c9a6777af46e2d598f6dd0d63057ab7a3
natspindola <natspindola@hotmail.com>
2022-04-12 19:12:47 -0300

    métodos set classe pessoa

--- Digibank/Classes/Pessoa.cs
+++ Digibank/Classes/Pessoa.cs
@@ -13,5 +13,20 @@ namespace Digibank.Classes
         public string CPF { get; private set; }
         public string Senha { get; private set; }
         public IConta Conta { get; set; }
+
+        public void SetNome(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public void SetCPF(string cpf)
+        {
+            this.CPF = cpf;
+        }
+
+        public void SetSenha(string senha)
+        {
+            this.Senha = senha;
+        }
     }
 }
